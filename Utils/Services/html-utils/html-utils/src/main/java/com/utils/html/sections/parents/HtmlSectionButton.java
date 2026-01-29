e38dabc3182297b2d40f8a2b9efa4712f5e7350a
Iocsa, Valeriu-Ionut <valeriu.iocsa@vitesco.com>
2026-01-29 12:53:41 +0200

    updated dependencies

--- Utils/Services/html-utils/html-utils/src/main/java/com/utils/html/sections/parents/HtmlSectionButton.java
+++ Utils/Services/html-utils/html-utils/src/main/java/com/utils/html/sections/parents/HtmlSectionButton.java
@@ -1,11 +1,14 @@
 package com.utils.html.sections.parents;
 
+import com.utils.annotations.ApiMethod;
+
 public class HtmlSectionButton extends AbstractHtmlSectionParent {
 
 	public HtmlSectionButton() {
 		super("button");
 	}
 
+	@ApiMethod
 	public AbstractHtmlSectionParent addAttributeOnClick(
 			final String onClick) {
 		return addAttribute("onclick", onClick);
